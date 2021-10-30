19abed7f069cda1db76e639b5c8265c194a4ee37
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-30 11:01:18 +0300

    connected mongoDB and made the app run only after the DB is working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const app = express();
-const { PORT } = require('./constants');
+
 const routes = require('./routes');
+const { PORT } = require('./constants');
+const { initDatabase } = require('./config/database-config');
+
+const app = express();
 require('./config/hbs-config')(app);
 require('./config/express-config')(app);
 
 app.use(routes);
-
-
-
-
-
-app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}/`));
+initDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}/`));
+    })
+    .catch(err => {
+        console.log('Cannot connect database', err);
+    });
