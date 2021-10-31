3c263799bb077a48c2ef83a3853aaf49dab13d1f
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-31 10:56:41 +0200

    successfully add housing to the database

--- src/controllers/housing-controller.js
+++ src/controllers/housing-controller.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 
+const housingService = require('../services/housing-service');
 router.get('/', (req, res) => {
     res.render('housing', { title: "Housing page" });
 });
 
 router.get('/create', (req, res) => {
     res.render('housing/create');
-})
+});
+
+router.post('/create', async(req, res) => {
+    try {
+        await housingService.create(req.body)
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+    };
+});
 
 module.exports = router;
